eea5394814cd6b1734ac48363a76fa90c36d7f80
vaisakh b <vaisakh@vaisakhs-Air.domain.name>
2024-08-26 00:39:29 +0530

    updated helpers.

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -11,3 +11,29 @@ export const generateAccountName = (accounts: Array<IAccount>) => {
 	return "Inf Account";
 };
 
+export const generateAlias = (str: string): string => {
+	if (!str) return str;
+	let res = "";
+	for (const s of str.split(" ")) {
+		res += s[0];
+	}
+	return res;
+};
+
+// m / purpose' / coin_type' / account' / change / address_index
+
+export type TSymbol = "BTC" | "SOL" | "ETH";
+
+export const getDerivationPath = (symbol: TSymbol, account: number) => {
+	switch (symbol) {
+		case "BTC":
+			return { coinType: 0, derivationPath: `m/44'/0'/${account}'/0'` };
+		case "SOL":
+			return { coinType: 501, derivationPath: `m/44'/501'/${account}'/0'` };
+		case "ETH":
+			return { coinType: 60, derivationPath: `m/44'/60'/${account}'/0'` };
+		default:
+			throw new Error("Unknown Symbol: " + symbol);
+	}
+};
+
